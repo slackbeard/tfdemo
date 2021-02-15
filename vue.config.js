9167e23db63bdf28255fd16f03e1ea42196cc80e
Brian <slackbeard@protonmail.com>
2021-02-15 12:26:46 -0800

    Add compression webpack plugin to gzip js files

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  chainWebpack(config) {
+    //config.plugins.delete('prefetch');
+
+    config.plugin('CompressionPlugin')
+      .use(
+      CompressionPlugin,
+      [{
+        test: /\.js$/i,
+      }]
+      );
+  }
+};
